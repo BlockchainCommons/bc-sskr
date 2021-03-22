67f65411f2f2c949c57cca44c6309122e170bb7d
gorazdko <gorazdko@gmail.com>
2021-03-22 16:39:55 +0100

    fix compiling after build optimization

--- build_wasm.sh
+++ build_wasm.sh
@@ -9,6 +9,6 @@ git submodule update --init
 emcc -Os -I./deps/bc-shamir/src/ -I./deps/bc-crypto-base/src/ \
 src/encoding.c deps/bc-shamir/src/shamir.c deps/bc-shamir/src/interpolate.c deps/bc-shamir/src/hazmat.c \
 deps/bc-crypto-base/src/sha2.c deps/bc-crypto-base/src/hmac.c deps/bc-crypto-base/src/memzero.c \
- -s WASM=1 -o wasm/sskr.js -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']" \
+ -s WASM=1 -o wasm/sskr.js -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap', 'addFunction', 'setValue']" \
  -s EXPORTED_FUNCTIONS="['_sskr_count_shards', '_sskr_generate', '_sskr_combine', '_malloc']" \
  -s ALLOW_MEMORY_GROWTH=1 -s ALLOW_TABLE_GROWTH \
